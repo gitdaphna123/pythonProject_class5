aac5653d6dbbb297b6bb295b6bd88a0389d7a6b7
gitdaphna123 <daphna.hh@gmail.com>
2023-03-08 20:18:08 +0200

    d

--- main.py
+++ main.py
@@ -1,26 +1,2 @@
-class Shark:
-    def __init__(self,name,age,color):
-        self.name = name
-        self.age = age
-        self.color = color
-
-    def swim(self):
-        print("the shark is swiming")
-
-    def eat(self):
-        print("the shark is eating")
-
-def main():
-    x = Shark('yossi','4','blue')
-    x.swim()
-    x.eat()
-    print(x.name)
-
-main()
-
-print("hi")
-print("yes")
-print("bel")
-print("g")
 print("2")
 print("e")
